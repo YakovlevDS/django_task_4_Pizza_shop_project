5889bdfda6ca1e9365ca129b003f5dbecb23dd83
Dmitriy Yakovlev <1d3ddd@gmail.com>
2022-03-12 18:56:06 +0200

    feat:add forms

--- pizzashopapp/views.py
+++ pizzashopapp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 
+from pizzashopapp.forms import UserForm, PizzaShopForm
 # Create your views here.
 def home(request):
     return redirect(pizzashop_home)
@@ -10,4 +11,9 @@ def pizzashop_home(request):
     return render(request, 'pizzashop/home.html', {})
 
 def pizzashop_sign_up(request):
-    return render(request, 'pizzashop/sign_up.html', {}) 
+    user_form = UserForm()
+    pizzashop_form = PizzaShopForm()
+    return render(request, 'pizzashop/sign_up.html', {
+        'user_form': user_form,
+        'pizzashop_form': pizzashop_form
+    }) 
